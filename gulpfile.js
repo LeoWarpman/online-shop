3009de055feb022874cc6be557ccb0e71bdd1ae8
LeoWarpman <93464005+LeoWarpman@users.noreply.github.com>
2022-01-22 12:24:12 +0400

    gulp include add

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,17 @@ global.app = {
 // Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js"
 
 // Наблюдатель изменений в файлах
+
 function watcher() {
-  gulp.watch(path.watch.files, copy)
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, copy);
 }
 
+const mainTasks = gulp.parallel(copy, html);
+
 // Построение сценариев 
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(reset, mainTasks, watcher);
 gulp.task('default', dev);
